5764f67708022c135307a3683cebf6a6eba336f1
okayamiami <knz2374471@stu.o-hara.ac.jp>
2024-10-31 13:55:36 +0900

    dao2oka

--- src/dao/AbsenceDao.java
+++ src/dao/AbsenceDao.java
@@ -1,5 +1,18 @@
 package dao;
 
-public class AbsenceDao {
+import bean.Absence;
+
+public class AbsenceDao extends Dao{
+
+	private String baseSql = "select * from Absence ";
+
+	public Absence getAbsenceInfo(String absence_id, String facility_id) throws Exception {
+
+	}
+
+	public void saveAbsenceInfo() throws Exception {
+
+	}
+
 
 }
