063d79c3d4fafccf417d32981e467dd76c4be708
2374551 <2374551@10.251.146.67>
2024-10-31 13:51:59 +0900

    bean/

--- src/bean/Absence.java
+++ src/bean/Absence.java
@@ -2,4 +2,41 @@ package bean;
 
 public class Absence {
 
+	private String absence_id;
+	private String absence_main;
+	private String child_id;
+	private String absence_date;
+	private String facility_id;
+
+	public String getAbsence_id() {
+		return absence_id;
+	}
+	public void setAbsence_id(String absence_id) {
+		this.absence_id = absence_id;
+	}
+	public String getAbsence_main() {
+		return absence_main;
+	}
+	public void setAbsence_main(String absence_main) {
+		this.absence_main = absence_main;
+	}
+	public String getChild_id() {
+		return child_id;
+	}
+	public void setChild_id(String child_id) {
+		this.child_id = child_id;
+	}
+	public String getAbsence_date() {
+		return absence_date;
+	}
+	public void setAbsence_date(String absence_date) {
+		this.absence_date = absence_date;
+	}
+	public String getFacility_id() {
+		return facility_id;
+	}
+	public void setFacility_id(String facility_id) {
+		this.facility_id = facility_id;
+	}
+
 }
--- src/bean/Get.java
+++ src/bean/Get.java
@@ -2,4 +2,33 @@ package bean;
 
 public class Get {
 
+	private int bus_id;
+	private String child_id;
+	private boolean get_is_attend;
+	private String facility_id;
+	public int getBus_id() {
+		return bus_id;
+	}
+	public void setBus_id(int bus_id) {
+		this.bus_id = bus_id;
+	}
+	public String getChild_id() {
+		return child_id;
+	}
+	public void setChild_id(String child_id) {
+		this.child_id = child_id;
+	}
+	public boolean isGet_is_attend() {
+		return get_is_attend;
+	}
+	public void setGet_is_attend(boolean get_is_attend) {
+		this.get_is_attend = get_is_attend;
+	}
+	public String getFacility_id() {
+		return facility_id;
+	}
+	public void setFacility_id(String facility_id) {
+		this.facility_id = facility_id;
+	}
+
 }
