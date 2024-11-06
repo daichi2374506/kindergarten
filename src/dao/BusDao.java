ff4947bd470383aeed85fe2ec667ae0a6c66c4c2
Sato9619 <knz2374582@stu.o-hara.ac.jp>
2024-11-06 10:14:21 +0900

    BasDao,ClassDao更新

--- src/dao/BusDao.java
+++ src/dao/BusDao.java
@@ -6,20 +6,66 @@
 
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import bean.Bus;
 
 public class BusDao extends Dao {
 
-	public List<Bus> getBus() throws Exception {
+	private String baseSql = "select * from Bus WHERE facility_id = ? ";
+
+	public List<Bus> getBus(String facility_id) throws Exception {
+		//戻り値用のリスト
+		List<Bus> list = new ArrayList<>();
+		Connection connection = getConnection();
+		PreparedStatement st = null;
+
+		try{
+			st = connection.prepareStatement(baseSql);
+			st.setString(1,facility_id);
+			ResultSet rSet = st.executeQuery();
+			Bus bus = new Bus();
+			while(rSet.next()){
+				bus.setBus_id(rSet.getString("bus_id"));
+				bus.setBus_name(rSet.getString("bus_name"));
+				bus.setFacility_id(rSet.getString("facility_id)"));
+				list.add(bus);
+			}
+		}catch(Exception e){
+			throw e;
+		} finally {
+			//
+			if(st != null) {
+				try {
+					st.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+
+			if(connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
 
 
+		}
+		return list;
 	}
 
-	public boolean saveBus() throws Exception{
 
-
-	}
+//	/
+//	public boolean saveBus() throws Exception{
+//
+//
+//	}
 
 }
