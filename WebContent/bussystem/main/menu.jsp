e14efee3fd2f688236dfa3a150bf04c7927441c3
2374551 <2374551@10.251.146.67>
2024-11-06 10:33:48 +0900

    ログイン

--- WebContent/bussystem/main/menu.jsp
+++ WebContent/bussystem/main/menu.jsp
@@ -4,7 +4,8 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>こにちわ</title>
+<title>こんにちわ</title>
+
 </head>
 <body>
 
--- src/bussystem/LoginExecuteAction.java
+++ src/bussystem/LoginExecuteAction.java
@@ -16,40 +16,43 @@ public class LoginExecuteAction extends Action{
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
-		System.out.println("AAAAA1");
-
 		String url = "";
 
 		Map<String, String> errors = new HashMap<>();
 
-		System.out.println("AAAAA1-2");
-
-		//
 		ManageUser MU = new ManageUser();
 
-		System.out.println("AAAAA1-3");
-
 		ManageUserDao MUDao = new ManageUserDao();
-
-		System.out.println("AAAAA2");
 
 		String user_id = req.getParameter("user_id");
 		String user_pass = req.getParameter("user_pass");
 		String facility_id = req.getParameter("facility_id");
 
-		System.out.println("AAAAA3");
-
 		MU = MUDao.login(user_id, user_pass, facility_id);
 
-		System.out.println("AAAAA4");
 
 		if(MU==null) {
 			errors.put("null", "ログインに失敗しました。IDまたはパスワードが正しくありません。");
-		} else {
+
+			System.out.println("AAAAA1");
+
+		} else if(user_id.contains("M")) {
 			MU.setAuthenticated(true);
+
+			System.out.println("AAAAA2");
+
+		} else if(user_id.contains("T")) {
+			MU.setAuthenticated(true);
+
+			System.out.println("AAAAA3");
+
+		} else if(user_id.contains("P")) {
+			MU.setAuthenticated(true);
+
+			System.out.println("AAAAA4");
+
 		}
 
-		System.out.println("AAAAA5");
 
 		if(!errors.isEmpty()) {
 			req.setAttribute("errors", errors);
@@ -57,13 +60,9 @@ public class LoginExecuteAction extends Action{
 			return;
 		}
 
-		System.out.println("AAAAA6");
-
 		HttpSession session = req.getSession(true);
 
 		session.setAttribute("user", MU);
-
-		System.out.println("AAAAA7");
 
 		url = "main/Menu.action";
 		res.sendRedirect(url);
