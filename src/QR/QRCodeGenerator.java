ba21170dbbc31c491c39263cac1b4d6d33b2e12e
okayamiami <knz2374471@stu.o-hara.ac.jp>
2024-11-06 14:53:20 +0900

    o

--- src/QR/QRCodeGenerator.java
+++ src/QR/QRCodeGenerator.java
@@ -27,7 +27,7 @@ public class QRCodeGenerator {
 
     public static void main(String[] args) {
         try {
-            generateQRCodeImage("2400001", 350, 350, QR_CODE_IMAGE_PATH);
+            generateQRCodeImage("https://plum-chloride.jp/kotonoha-tango/index.html?appsule=2", 350, 350, QR_CODE_IMAGE_PATH);
         } catch (WriterException | IOException e) {
             e.printStackTrace();
         }
