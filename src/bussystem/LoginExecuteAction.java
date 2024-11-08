074e2580715cf5f75f114164d936d80ea2c6dcbe
kz0727 <knz2374550@stu.o-hara.ac.jp>
2024-11-08 09:58:42 +0900

    login修正

--- src/bussystem/LoginExecuteAction.java
+++ src/bussystem/LoginExecuteAction.java
@@ -18,7 +18,6 @@ public class LoginExecuteAction extends Action{
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
-<<<<<<< HEAD
 		HttpSession session = req.getSession(true);
 
 		String url = "";
@@ -33,141 +32,35 @@ public class LoginExecuteAction extends Action{
 		String user_pass = req.getParameter("user_pass");
 		String facility_id = req.getParameter("facility_id");
 
-		if(user_id.contains("M") || ) (user_id.contains("T"){
+		if(user_id.contains("M") || user_id.contains("T")){
 			MU = MUDao.login(user_id, user_pass, facility_id);
+			if(MU == null){
+				errors.put("kome", "ログインに失敗しました。IDまたはパスワードが違います。");
+				req.setAttribute("errors", errors);
+				req.getRequestDispatcher("login.jsp").forward(req, res);
+			} else {
+				MU.setAuthenticated(true);
+				//セッションに"user"という変数名で値はTeacher変数の中身
+				session.setAttribute("user", MU);
+				url = "main/Menu.action";
+				res.sendRedirect(url);
+			}
+
 
 		} else if(user_id.contains("P")) {
-			PU = PUDao.login(user_id, user_pass, facility_id);
+			PU = PUDao.parentsLogin(user_id, user_pass, facility_id);
+			if(PU == null){
+				errors.put("kome", "ログインに失敗しました。IDまたはパスワードが違います。");
+				req.setAttribute("errors", errors);
+				req.getRequestDispatcher("login.jsp").forward(req, res);
+			} else {
+				PU.setAuthenticated(true);
+				session.setAttribute("user", PU);
+				url = "main/Menu.action";
+				res.sendRedirect(url);
+			}
 		}
-
-
-
-
-//		if(MU==null) {
-//			errors.put("null", "ログインに失敗しました。IDまたはパスワードが正しくありません。");
-//
-//			System.out.println("AAAAA1");
-//
-//		} else if(user_id.contains("M")) {
-//			MU.setAuthenticated(true);
-//
-//			String m = "m";
-//			session.setAttribute("m", m);
-//
-//			System.out.println(m);
-//
-//			System.out.println("管理者としてログイン");
-//
-//		} else if(user_id.contains("T")) {
-//			MU.setAuthenticated(true);
-//
-//			String t = "t";
-//			session.setAttribute("t", t);
-//
-//			System.out.println("先生としてログイン");
-//
-//		} else if(user_id.contains("P")) {
-//			MU.setAuthenticated(true);
-//
-//			String p = "p";
-//			session.setAttribute("p", p);
-//
-//			System.out.println("保護者としてログイン");
-//
-//		}
-//
-//
-//		if(!errors.isEmpty()) {
-//			req.setAttribute("errors", errors);
-//			req.getRequestDispatcher("login.jsp").forward(req, res);
-//			return;
-//		}
-//
-//		session.setAttribute("user", MU);
-//
-//		url = "main/Menu.action";
-//		res.sendRedirect(url);
-//
-//
-//	}
-//
-//}
-=======
-		String url = "";
-
-		Map<String, String> errors = new HashMap<>();
-
-		ManageUser MU = new ManageUser();
-		ParentsUser PU = new ParentsUser();
-
-		ManageUserDao MUDao = new ManageUserDao();
-		ParentsUserDao PUDao = new ParentsUserDao();
-
-		HttpSession session = req.getSession(true);
-
-		String user_id = req.getParameter("user_id");
-		String user_pass = req.getParameter("user_pass");
-		String facility_id = req.getParameter("facility_id");
-
-		MU = MUDao.login(user_id, user_pass, facility_id);
-
-		String parents_id = req.getParameter("parents_id");
-		String parents_pass = req.getParameter("parents_pass");
-
-		PU = PUDao.parentsLogin(parents_id, parents_pass, facility_id);
-
-
-		if(MU==null) {
-			errors.put("null", "ログインに失敗しました。IDまたはパスワードが正しくありません。");
-
-			System.out.println("ログインに失敗しました/管理者or先生");
-
-		} else if(user_id.contains("M")) {
-			MU.setAuthenticated(true);
-
-			String m = "m";
-			session.setAttribute("m", m);
-
-			System.out.println(m);
-
-			System.out.println("管理者としてログイン");
-
-		} else if(user_id.contains("T")) {
-			MU.setAuthenticated(true);
-
-			String t = "t";
-			session.setAttribute("t", t);
-
-			System.out.println("先生としてログイン");
-		}
-
-		if(PU==null) {
-			errors.put("null", "ログインに失敗しました。IDまたはパスワードが正しくありません。");
-
-			System.out.println("ログインに失敗しました/保護者");
-
-		} else {
-			PU.setAuthenticated(true);
-
-			String p = "p";
-			session.setAttribute("p", p);
-			System.out.println(p);
-			System.out.println("保護者としてログイン");
-		}
-
-		if(!errors.isEmpty()) {
-			req.setAttribute("errors", errors);
-			req.getRequestDispatcher("login.jsp").forward(req, res);
-			return;
-		}
-
-		session.setAttribute("user", MU);
-
-		url = "main/Menu.action";
-		res.sendRedirect(url);
-
-
 	}
-
 }
->>>>>>> branch 'master' of https://github.com/daichi2374506/kindergarten.git
+
+
