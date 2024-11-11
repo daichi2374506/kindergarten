7e139e14aa3c5de8aa941c1444f922e2c4e32ce8
okayamiami <knz2374471@stu.o-hara.ac.jp>
2024-11-11 12:09:10 +0900

    qqq

--- src/QR/QRCodeGenerator.java
+++ src/QR/QRCodeGenerator.java
@@ -12,7 +12,7 @@ import com.google.zxing.qrcode.QRCodeWriter;
 
 public class QRCodeGenerator {
 
-    private static final String QR_CODE_IMAGE_PATH = "./Webcontent/syashin/MyQRCode.png";
+    private static final String QR_CODE_IMAGE_PATH = "./MyQRCode.png";
 
     private static void generateQRCodeImage(String text, int width, int height, String filePath)
             throws WriterException, IOException {
@@ -20,14 +20,12 @@ public class QRCodeGenerator {
         BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
 
         Path path = FileSystems.getDefault().getPath(filePath);
-        System.out.println("Writing QR code to: " + path.toAbsolutePath());
         MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
-        System.out.println("QR code generated successfully.");
     }
 
     public static void main(String[] args) {
         try {
-            generateQRCodeImage("https://plum-chloride.jp/kotonoha-tango/index.html?appsule=2", 350, 350, QR_CODE_IMAGE_PATH);
+            generateQRCodeImage("Hello, World!", 350, 350, QR_CODE_IMAGE_PATH);
         } catch (WriterException | IOException e) {
             e.printStackTrace();
         }
